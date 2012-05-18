b729bc42c21c3e60a343fe1ccc02ff133d7c0dfb
Sven Heinicke <sven@genomics.princeton.edu>
2012-05-18 13:21:49 -0400

    All implmented features seem to be working

--- BGV/SVGform.js
+++ BGV/SVGform.js
@@ -1,87 +1,99 @@
 BGV.form=function(prefix,defaults,go){
-  this.prefix=prefix;
+  this.prefix=prefix; // kinda like the form name
   this._v={};
-  
+
   // look for radio buttons
-  var radio=document.getElementsByClassName('formRadio');
-  for(var i=0;i<radio.length;i++){
-    var r=radio[i];
-    if(this.ours(r)){
-      var buttons=r.getElementsByTagName('tspan');
-      var name=this.name(r);
+  var tags=document.getElementsByClassName('formRadio');
+  for(var i=0;i<tags.length;i++){
+    var rb=tags[i];
+    if(this.ours(rb)){
+      var name=this.name(rb);
+      var radio=[];
+      for(var n=rb.firstChild;n!=null;n=n.nextSibling){
+	if('tspan'==n.nodeName){
+	  radio.push(n);
+	}
+      }
+
+      // get the textContent sans the ballot box
+      var value=function(tag){
+	var out='';
+	for(var n=tag.firstChild;n!=null;n=n.nextSibling){
+	  if('#text'==n.nodeName){
+	    out+=n.textContent;
+	  }
+	}
+	return out.trim();
+      }
 
       if(undefined==defaults[name]){
-	// no defaults to set, just get the value
-	for(var j=0;j<buttons.length;j++){
-	  var b=buttons[j];
-	  // for now just assume the frist character is the toggle
+	for(var j=0;j<radio.length;j++){
+	  var b=radio[j].getElementsByTagName('tspan')[0];
 	  if(this.isChecked(b)){
-	    this.set(r,b.textContent.trim().substring(1));
+	    this.set(rb,value(radio[j]));
 	  }
 	}
       }else{
-	// we have a default to set!
-	var def=defaults[name].trim().toLowerCase();
-
-	for(var j=0;j<buttons.length;j++){
-	  var b=buttons[j];
-	  var t=b.textContent.trim().substring(1);
-	  if(t.toLowerCase()==def){
-	    b.textContent=this._true+t;
+	// change the SVG to reflecta  given default
+	var def=defaults[name].trim().toLowerCase();	
+	for(var j=0;j<radio.length;j++){
+	  var b=radio[j].getElementsByTagName('tspan')[0];
+	  if(value(radio[j]).toLowerCase()==def){
+	    b.textContent=this._true;
 	  }else{
-	    b.textContent=this._false+t;
+	    b.textContent=this._false;
 	  }
 	}
       }
 
+
       // make it clickable
       var that=this;
-      for(var j=0;j<buttons.length;j++){
-	var b=buttons[j];
-	b.onclick=function(){
-	  if(that.isChecked(this)){
+      for(var j=0;j<radio.length;j++){
+	radio[j].onclick=function(){
+	  if(that.isChecked(this.getElementsByTagName('tspan')[0])){
 	    // if we are checked, do nothing
 	    return;
 	  }
-	  for(var k=0;k<buttons.length;k++){
-	    var bk=buttons[k];
-	    if(that.isChecked(bk)){
-	      that.toggle(bk);
-	    }else if(this==bk){
-	      that.toggle(bk);
-	      that.set(r,bk.textContent.trim().substring(1));
+	  for(var k=0;k<radio.length;k++){
+	    var b=radio[k].getElementsByTagName('tspan')[0];
+	    if(that.isChecked(b)){
+	      that.toggle(b);
+	    }else if(this==radio[k]){
+	      that.toggle(b);
+	      that.set(rb,value(radio[k]));
 	      go(that);
 	    }
-
 	  }
-	}
-      }
 
+	}//function
+      }
     }
   }
+
   
   // look for check boxes
-  var checkbox=document.getElementsByClassName('formCheckbox');
-  for(var i=0;i<checkbox.length;i++){
-    var c=checkbox[i];
+  var tags=document.getElementsByClassName('formCheckbox');
+  for(var i=0;i<tags.length;i++){
+    var cb=tags[i];
 
-    if(this.ours(c)){
-      var name=this.name(c);
+    if(this.ours(cb)){
+      var name=this.name(cb);
+      var tog=cb.getElementsByTagName('tspan')[0];
+
       if(undefined==defaults[name]){
-	this.set(c,this.isChecked(c));
+	// record SVG says is set
+	this.set(cb,this.isChecked(tog));
       }else{
-	var v=c.textContent.trim().substring(1);
-	if(defaults[name]){
-	  c.textContent=this._true+v;
-	}else{
-	  c.textContent=this._false+v;
-	}
+	// set the value from the default
+	tog.textContent=defaults[name]?this._true:this._false;
       }
 
       // make it clickable
       var that=this;
-      c.onclick=function(){
-	that.set(this,that.toggle(this));
+      cb.onclick=function(){
+	var tog=this.getElementsByTagName('tspan')[0];
+	that.set(this,that.toggle(tog));
 	go(that);
       }
 
@@ -95,44 +107,45 @@ BGV.form.prototype={
   _false:'☐',
   _dil:'-', // delimiter
 
-  isChecked:function(tag){
-    // for now just assume the first character is a checkbox 
-    return tag.textContent.trim()[0]==this._true;
+  values:function(){
+    return this._v;
   },
 
-  // Toggle a box, and return the status it was toggled to
-  toggle:function(tag){
-    var txt=tag.textContent.trim().substring(1);
-    if(this.isChecked(tag)){
-      tag.textContent=this._false+txt;
-      return false;
-    }
-    tag.textContent=this._true+txt;
-    return true;
+  // // //
+  // tag in these functions means where class="form*"
+
+  // Returns true if this is a tag we are worrying about
+  ours:function(tag){
+    var p=this.prefix+this._dil;
+    return tag.hasAttribute('id')&&
+      (tag.getAttribute('id').substring(p.length,0)==p)
   },
 
   name:function(tag){
     var p=this.prefix+this._dil;
-    return tag.getAttribute('id').trim().substring(p.length);
+    return tag.getAttribute('id').substring(p.length);
   },
 
   set:function(tag,value){
     this._v[this.name(tag)]=value;
   },
 
-  // Returns true if this is a tag we are worrying about
-  ours:function(tag){
-    var p=this.prefix+this._dil;
-    if(tag.hasAttribute('id')&&
-       (tag.getAttribute('id').substring(p.length,0)==p)){
-      return true;
-    }
-    return false;
+  // // //
+  // The rest of the functions take the tspan with the Unicode ballot
+  // box in it, not the ane wich class="form*"
+
+  isChecked:function(tag){
+    return tag.textContent==this._true;
   },
 
-  
-  values:function(){
-    return this._v;
+  // Toggle a box, and return the status it was toggled to
+  toggle:function(tag){
+    if(this.isChecked(tag)){
+      tag.textContent=this._false;
+      return false;
+    }
+    tag.textContent=this._true;
+    return true;
   }
 
 };
