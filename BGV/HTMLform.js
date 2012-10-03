c32262c2b02f118bd99bfa0e9dd1d8fec27dcbbc
Sven Heinicke <sven@genomics.princeton.edu>
2012-10-03 15:07:14 -0400

    got get & phy toggling going

--- BGV/HTMLform.js
+++ BGV/HTMLform.js
@@ -1,16 +1,38 @@
 BGV.Form=function(name,go){
-//  this.go=go;
+  
+
   for(var l=0;l<document.forms.length;l++){
-    if(document.forms[l].name===name){
-//      this.tags=document.forms[l].getElementsByTagName('input');
-      this.tags=document.forms[l].elements;
-      l=document.forms.length+1;
+    var f=document.forms[l]
+    if(f.name===name){
+      l=document.forms.length+1; // last
+      this.tags=f.elements;
     }
   }
   
+  // currently only work for toggling select-multi elements
+  var toggle=function(){
+    for(var m=0;m<this.BGVtoggles.length;m++){
+      this.BGVtoggles[m].selected=this.checked;
+    }
+    go();
+  }
+
   for(var l=0;l<this.tags.length;l++){
     var t=this.tags[l];
-    t.onchange=go;
+    if(''!==t.name){
+      t.onchange=go;
+    }
+    
+    if(t.classList.contains('toggle')){
+      var p=t.parentNode;
+      var fs=p;
+      while('FIELDSET'!=fs.tagName){
+	fs=p.parentNode;
+      }
+      t.BGVtoggles=fs.getElementsByClassName(p.textContent.toLowerCase());
+      t.onclick=toggle;
+    }
+
   }
 };
 
